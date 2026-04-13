22a995e68b6280b2e10f3ab369cad0768f3cd4c5
mario <mariotadic246@gmail.com>
2026-04-13 13:54:29 +0200

    Sensor testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
 #include <Arduino.h>
 
 const byte MOTOR_LEFT_SPEED = 9;
-const byte MOTOR_LEFT_FORWARD = 4;
-const byte MOTOR_LEFT_BACKWARD = 7;
+const byte MOTOR_LEFT_FORWARD = 7;
+const byte MOTOR_LEFT_BACKWARD = 4;
 const byte MOTOR_RIGHT_SPEED = 10;
-const byte MOTOR_RIGHT_FORWARD = 8;
-const byte MOTOR_RIGHT_BACKWORD = 12;
+const byte MOTOR_RIGHT_FORWARD = 12;
+const byte MOTOR_RIGHT_BACKWORD = 8;
+
+const int IR_SENSOR_FRONT_LEFT = A0;
+
 
 void setup() {
+  Serial.begin(115200);
   pinMode(MOTOR_LEFT_SPEED, OUTPUT);
   pinMode(MOTOR_LEFT_FORWARD, OUTPUT);
   pinMode(MOTOR_LEFT_BACKWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_SPEED, OUTPUT);
   pinMode(MOTOR_RIGHT_FORWARD, OUTPUT);
   pinMode(MOTOR_RIGHT_BACKWORD, OUTPUT);
+  pinMode(IR_SENSOR_FRONT_LEFT, INPUT);
 }
 
 void loop() {
+  const int val= analogRead(A0);
+  Serial.println(val);
+
+  delay(100);
+/*
   // Vorwärts
   digitalWrite(MOTOR_LEFT_FORWARD, HIGH);
   digitalWrite(MOTOR_LEFT_BACKWARD, LOW);
@@ -50,4 +60,5 @@ void loop() {
   analogWrite(MOTOR_RIGHT_SPEED, 0);
 
   delay(500);
-  }
+  */
+}
